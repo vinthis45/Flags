2e797877fa016faeec18f79fabf68a8f74c02703
Abinash <asavisingh72@gmail.com>
2023-12-30 15:05:58 +0530

    final commit

---
+++ countries/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import React, { useEffect, useState } from "react";
+
+
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data: ", error))
+  }, [])
+
+  return (
+    <div className="App">
+      {countries.length ?
+        countries.map((country) => (
+          <div key={country.cca3} className="cardStyle">
+            <img
+              src={country.flags.png}
+              className="imgStyle"
+              alt={`Flag of ${country.name.common}`}
+            />
+            <h2>{country.name.common}</h2>
+          </div>
+        )) : (
+          <div>Loading...</div>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
